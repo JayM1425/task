fbc8e1240c122e008c1479b60ad101f2af90d765
JayM1425 <mkali25taa@gmail.com>
2025-05-25 20:37:34 +0300

    updated index file

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Admin Panel</title>
+    <title>MUST Keys Management System.</title>
 </head>
 
 <body>
@@ -16,7 +16,7 @@
         <div class="header">
             <div class="nav">
                
-                <h1 class="search">MUST KEYS MANAGEMENT SYSTEM</h1>
+                <h1 class="search">Admin Panel</h1>
                 <div class="user">
                     <a href="#" id="openModalBtn" class="btn">Add New</a>
 
